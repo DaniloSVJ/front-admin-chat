e7c59e76feb8da113bfd0a10789c66f5abdc62fb
JeffersonDomingos <dominguesmorais@hotmail.com>
2024-06-25 16:17:25 -0300

    atz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 
-function App() {
+const socket = io('http://localhost:4000');
+
+function UserApp() {
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState('');
+  const [joined, setJoined] = useState(false);
+
+  useEffect(() => {
+    socket.on('msgtoSuport', (message) => {
+      alert(message)
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+  }, []);
+  socket.on('msgtoSuport', (message) => {
+    console.log(message);
+    setMessages((prevMessages) => [...prevMessages, message]);
+  });
+  const joinChat = () => {
+    if (username) {
+      socket.emit('join', username);
+      setJoined(true);
+    }
+  };
+
+  const sendMessage = () => {
+    if (message) {
+      socket.emit('message', { username, message });
+      setMessage('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {!joined ? (
+        <div>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <button onClick={joinChat}>Join Chat</button>
+        </div>
+      ) : (
+        <div>
+          <div>
+            {messages.map((msg, index) => (
+              <div key={index}>
+                <strong>{msg.username}</strong>: {msg.message}
+              </div>
+            ))}
+          </div>
+          <input
+            type="text"
+            placeholder="Enter your message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button onClick={sendMessage}>Send</button>
+        </div>
+      )}
     </div>
   );
 }
 
-export default App;
+export default UserApp;
