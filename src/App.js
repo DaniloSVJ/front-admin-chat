e3031a8c283bd41b04413bcd14e06fe0135d8279
JeffersonDomingos <dominguesmorais@hotmail.com>
2024-07-04 09:17:16 -0300

    atz

--- src/App.js
+++ src/App.js
@@ -1,51 +1,90 @@
-  import React, { useState, useEffect } from 'react';
-  import io from 'socket.io-client';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 
-  const socket = io('http://localhost:4000');
+const socket = io('http://localhost:4000');
 
-  function App() {
-    const [message, setMessage] = useState('');
-    const [messages, setMessages] = useState([]);
+function App() {
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [clientId, setClientId] = useState('');
+  const [supportId, setSupportId] = useState('' );
 
-    useEffect(() => {
-      socket.emit('joinRoom', { room: 'client' });
 
-      socket.on('clientMessage', (data) => {
-        setMessages((prevMessages) => [...prevMessages, `Support: ${data.message}`]);
-      });
-      
-      return () => {
-        socket.off('clientMessage');
-      };
-    }, []);
+  useEffect(() => {
+    socket.emit('joinRoom', { room: 'client', projectId: '20buscar' });
 
-    const sendMessage = () => {
-      if (message) {
-        
-        socket.emit('clientMessage', { message });
-        // alert(socket.id)
-        setMessage('');
-      }
+    socket.on('clientMessage', (data) => {
+      console.log(data)
+      setSupportId(data.supportId)
+      setMessages((prevMessages) => [...prevMessages, `Support: ${data.messages}`]);
+    });
+
+    return () => {
+      socket.off('clientMessage');
     };
+  }, []);
 
-    return (
-      <div>
-        <h1>Chat Client</h1>
-        <div id="messages">
-          {messages.map((msg, index) => (
-            <div key={index}>{msg}</div>
-          ))}
-        </div>
-        <input
-          id="message"
-          type="text"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          placeholder="Type your message here"
-        />
-        <button onClick={sendMessage}>Send</button>
+  const sendMessage = () => {
+    if (message) {
+      socket.emit('clientMessage', {
+        userType: 'contacts',
+        socketId: clientId,
+        projectId: clientId,
+        supportId: supportId,
+        messageType: 'message',
+        messages: message,
+        orige: 'monitor'
+      }
+      );
+
+
+    }
+  };
+
+  const sendNameCli = () => {
+    if (message) {
+      socket.emit('joinRoom', { room: 'client', projectId: clientId });
+
+    }
+  };
+
+
+  return (
+    <div>
+      <h1>Chat Client</h1>
+      <div id="messages">
+        {messages.map((msg, index) => (
+          <div key={index}>{msg}</div>
+        ))}
       </div>
-    );
-  }
+      <div style={{ flexDirection: 'column' }}>
+        <div style={{ marginBottom: '15px' }}>
 
-  export default App;
+          <input
+            id="message"
+            type="text"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="Type your message here"
+          />
+          <button onClick={sendMessage}>Send</button>
+        </div>
+        <div>
+
+          <input
+            id="client"
+            type="text"
+            value={clientId}
+            onChange={(e) => setClientId(e.target.value)}
+            placeholder="nome do cliente"
+          />
+          <button onClick={sendNameCli}>Send</button>
+
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
